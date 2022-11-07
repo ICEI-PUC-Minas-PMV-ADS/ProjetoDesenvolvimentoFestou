4a0cbb0d42b217bc8e8f9bb0cd09e3230aa0d12b
Isaac Gomes <1302300@sga.pucminas.br>
2022-11-06 22:59:03 -0300

    Att Company

--- src/frontweb/src/components/pages/Company.js
+++ src/frontweb/src/components/pages/Company.js
@@ -5,8 +5,9 @@ function Company() {
   return (
       <section >
           <h1 className={styles.title_container}>Sobre a empresa</h1>
-          <p className={styles.company_container}>A Festou é uma empresa responsável por organizar em um mesmo ambiente funcionalidades para auxiliar tanto Locatários que desejam divulgar seus espaços para eventos, quanto
-              Locadores que procuram pelo local ideal gggggggggggpara suas festas. </p>
+          <p className={styles.company_container}>
+              A Festou e uma empresa responsavel por organizar em um mesmo ambiente funcionalidades para auxiliar tanto Locatarios que desejam divulgar seus espacos para eventos, quanto
+              Locadores que procuram pelo local ideal para suas festas. </p>
       </section>
   );
 }
